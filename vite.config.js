63711549a5170195e1ded4f4fa841659d7f3ce17
satish-highlevel <satish.kumar@gohighlevel.com>
2025-12-23 00:26:01 +0530

    Added deployment plan for chat app

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: process.env.VITE_PORT || process.env.PORT || 4173,
-    host: '0.0.0.0'
+    host: "0.0.0.0",
   },
   preview: {
     port: process.env.VITE_PORT || process.env.PORT || 4173,
-    host: '0.0.0.0'
-  }
-})
+    host: "0.0.0.0",
+    allowedHosts: [
+      "stranger-chat-web-production-22ae.up.railway.app",
+      "https://fabulous-granita-a6b8ba.netlify.app",
+      "https://strangerchat.co.in",
+    ],
+  },
+});
